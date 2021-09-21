871286b4f89cccbaa94b1e89d5390f424fae2e64
Katherine Wise <k06kw02@gmail.com>
2021-09-21 14:10:03 +0100

    installed shoulda gem for association testing

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,7 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
+
   context 'validation tests' do
+    current_user = User.first_or_create!(email: 'ext@example.com', password: 'password', password_confirmation: 'password')
     it 'ensures title presence' do
       post = Post.new(content: 'stuff', date: '09092021').save
       expect(post).to eq(false)
@@ -18,7 +20,7 @@ RSpec.describe Post, type: :model do
     end
 
     it 'saves successfully' do
-      post = Post.new(title: 'stuff', content: 'stuff', date: '09092021').save
+      post = Post.new(title: 'stuff', content: 'stuff', date: '09092021', user: current_user).save
       expect(post).to eq(true)
     end
 
