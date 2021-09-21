94b2f0d5437e640a1b44bb9f59ff5617b7a79bf3
Katherine Wise <k06kw02@gmail.com>
2021-09-21 11:11:27 +0100

    validations post

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'validation tests' do
+    it 'ensures title presence' do
+      post = Post.new(content: 'stuff', date: '09092021').save
+      expect(post).to eq(false)
+    end
+
+    it 'ensures content presence' do
+      post = Post.new(title: 'stuff', date: '09092021').save
+      expect(post).to eq(false)
+    end
+
+    it 'ensures date presence' do
+      post = Post.new(title: 'stuff', content: 'stuff').save
+      expect(post).to eq(false)
+    end
+
+    it 'saves successfully' do
+      post = Post.new(title: 'stuff', content: 'stuff', date: '09092021').save
+      expect(post).to eq(true)
+    end
+
+  end
 end
