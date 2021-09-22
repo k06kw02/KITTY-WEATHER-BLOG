4ffb0b5fe435cf3396e122271f2fcf6f9f66ba23
Katherine Wise <k06kw02@gmail.com>
2021-09-22 10:50:22 +0100

    added controller/views tests for post

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,7 +1,151 @@
 require 'rails_helper'
 
-RSpec.describe "Posts", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+RSpec.describe '/posts', type: :request do
+  # Post. As you add validations to Post, be sure to
+  # adjust the attributes here as well.
+  current_user = User.first_or_create!(email: 'dean@example.com', password: 'password', password_confirmation: 'password')
+  content_text = "this is a reasonable amount of text for a blog post"
+
+  let(:valid_attributes) do
+    {
+      'id' => '1',
+      'title' => 'Test',
+      'content' => content_text,
+      'user' => current_user
+    }
+  end
+
+  let(:invalid_attributes) do
+    {
+      'id' => 'a',
+      'title' => '1',
+      'content' => '1234'
+    }
+  end
+
+  describe 'GET /index' do
+    it 'renders a successful response' do
+      post = Post.new(valid_attributes)
+      post.user = current_user
+      post.save
+      get posts_url
+      expect(response).to be_successful
+    end
+  end
+
+  describe 'GET /show' do
+    it 'renders a successful response' do
+      post = Post.new(valid_attributes)
+      post.user = current_user
+      post.save
+      get post_url(post)
+      expect(response).to be_successful
+    end
+  end
+
+  describe 'GET /new' do
+    it 'renders a successful response' do
+      get new_post_url
+      expect(response).to be_successful
+    end
+  end
+
+  describe 'GET /edit' do
+    it 'render a successful response' do
+      post = Post.new(valid_attributes)
+      post.user = current_user
+      post.save
+      get edit_post_url(post)
+      expect(response).to be_successful
+    end
+  end
+
+  describe 'POST /create' do
+    context 'with valid parameters' do
+      it 'creates a new Post' do
+        expect do
+          post = Post.new(valid_attributes)
+          post.user = current_user
+          post.save
+          post posts_url, params: { post: valid_attributes }
+        end.to change(Post, :count).by(1)
+      end
+
+      it 'redirects to the created post' do
+        post posts_url, params: { post: valid_attributes }
+        expect(response).to be_successful
+      end
+    end
+
+    context 'with invalid parameters' do
+      it 'does not create a new Post' do
+        expect do
+          post posts_url, params: { post: invalid_attributes }
+        end.to change(Post, :count).by(0)
+      end
+
+      it "renders a successful response (i.e. to display the 'new' template)" do
+        post posts_url, params: { post: invalid_attributes }
+        expect(response).to be_successful
+      end
+    end
+  end
+
+  describe 'PATCH /update' do
+    context 'with valid parameters' do
+      let(:new_attributes) do
+        {
+          id: '1',
+          title: 'Test',
+          content: content_text,
+          user: current_user
+        }
+      end
+
+      it 'updates the requested post' do
+        post = Post.new(valid_attributes)
+        post.user = current_user
+        post.save
+        patch post_url(post), params: { post: new_attributes }
+        post.reload
+        skip('Add assertions for updated state')
+      end
+
+      it 'redirects to the post' do
+        post = Post.new(valid_attributes)
+        post.user = current_user
+        post.save
+        patch post_url(post), params: { post: new_attributes }
+        post.reload
+        expect(response).to redirect_to(post_url(post))
+      end
+    end
+
+    context 'with invalid parameters' do
+      it "renders a successful response (i.e. to display the 'edit' template)" do
+        post = Post.create! valid_attributes
+        patch post_url(post), params: { post: invalid_attributes }
+        expect(response).to be_successful
+      end
+    end
+  end
+
+  describe 'DELETE /destroy' do
+    it 'destroys the requested post' do
+      post = Post.new(valid_attributes)
+      post.user = current_user
+      post.save
+      expect do
+        delete post_url(post)
+      end.to change(Post, :count).by(-1)
+    end
+
+    it 'redirects to the posts list' do
+      post = Post.new(valid_attributes)
+      post.user = current_user
+      post.save
+      delete post_url(post)
+      expect(response).to redirect_to(posts_url)
+    end
   end
 end
