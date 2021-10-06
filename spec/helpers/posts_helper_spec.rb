661dc74460bca7d3531691714214f31fb406cb48
Katherine Wise <k06kw02@gmail.com>
2021-10-06 11:38:41 +0100

    Revert "significant fixes, addressing tests, added index view"
    This reverts commit 9b836acd0acd7b867dbbb8b7bbd38a6413707d8d.

--- spec/helpers/posts_helper_spec.rb
+++ spec/helpers/posts_helper_spec.rb
@@ -10,16 +10,6 @@ require 'rails_helper'
 #     end
 #   end
 # end
-include PostsHelper
 RSpec.describe PostsHelper, type: :helper do
-  it 'assigns a user to a post' do
-    creator = User.first_or_create!(email: 'dean@example.com', password: 'password', password_confirmation: 'password')
-    @post = Post.new(
-      title: 'MyString',
-      content: 'MyText',
-      views: 1
-    )
-    assign_post_creator(@post, creator)
-    expect(@post.user).to be(creator)
-  end
+  pending "add some examples to (or delete) #{__FILE__}"
 end
